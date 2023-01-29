8c354b750ed36d29fad0ccac0a171d06c3f9cc5d
Padelvi <111744628+Padelvi@users.noreply.github.com>
2023-01-29 15:29:47 +0100

    Fix convert.py

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 from flask import render_template, Blueprint, redirect, url_for, request, abort
 from click import echo
+import formatter
 
 bp = Blueprint('convert', __name__, url_prefix='/convert')
 
